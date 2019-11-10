c75415d9044f8d935e3617e466c31f32cafe0ce6
Admin <guozengjie110@foxmail.com>
2019-11-11 06:07:55 +0800

    第一次提交

--- app/src/main/java/club/guozengjie/jetpack/Utils.kt
+++ app/src/main/java/club/guozengjie/jetpack/Utils.kt
@@ -18,7 +18,7 @@ object Utils {
     @JvmStatic
     fun loadImage(view: ImageView, imgUrl: String) {
         Glide.with(view)
-            .load("https://upload.jianshu.io/users/upload_avatars/6854876/aa73746f-9296-43a1-996e-609df65b1392?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240")
+            .load(imgUrl)
             .into(view)
     }
 }
